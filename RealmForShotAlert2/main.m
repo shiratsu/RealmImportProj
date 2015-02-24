4bc2ea0ec5fa042f40eacb5affebe6229d1418d3
hiratsukashu <hiratsukashu@indival.co.jp>
2015-02-24 11:51:34 +0900

    Initial Commit

---
+++ RealmForShotAlert2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RealmForShotAlert2
+//
+//  Created by 平塚　俊輔 on 2/24/15.
+//  Copyright (c) 2015 平塚　俊輔. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
